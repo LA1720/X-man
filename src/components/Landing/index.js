d2761d84c3db9292dcdb1bcfaa147f8889aa62e8
ALI AFZAL <layaafzal74@gmail.com>
2021-01-26 14:40:45 +0100

    griffe end

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,4 +1,4 @@
-import React, { useRef,useEffect, useState, Fragment } from 'react';
+import React, { useRef, useEffect, useState, Fragment } from 'react';
 
 
 const Landing = () => {
@@ -16,12 +16,32 @@ const Landing = () => {
         }, 1000);
     }, [])
 
+    const setLeftImg  = () => {
+
+        refWolverine.current.classList.add("leftImg"); 
+        
+    }
+
+    const setRightImg = () => {
+        refWolverine.current.classList.add("rightImg");
+    }
+
+    const clearImg = () => {
+        if(refWolverine.current.classList.contains("leftImg")) {
+            refWolverine.current.classList.remove("leftImg");
+        } else if (refWolverine.current.classList.contains("rightImg")){
+            refWolverine.current.classList.remove("rightImg");
+        }
+    }
+
     const displayBtn = btn && (
         <Fragment>
-        <div className="leftBox">
+        <div onMouseOver ={setLeftImg} onMosueOut = {clearImg}className="leftBox">
                 <button className="btn-welcome">Inscription</button>
             </div>
-            <div className="rightBox">
+            <div onMouseOver ={setRightImg} 
+            onMouseOut = {clearImg}
+            className="rightBox">
                 <button className="btn-welcome">Connexion</button>
             </div>
         </Fragment>
