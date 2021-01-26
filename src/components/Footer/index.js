0479c43c6303d4ea2359e30d6ac219bbbe85efce
ALI AFZAL <layaafzal74@gmail.com>
2021-01-26 12:06:52 +0100

    Folder, File format and placement

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>  
+        <div className="footer-container">
+            <p>Projet réalisé par LA - 2021</p>
+            <p>Les icônes Wolverien, Iron-man, Spider-man, Batman sont pris sur iconFinder.com  </p>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer
